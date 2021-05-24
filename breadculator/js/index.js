f21f918cc1539f97c31af2e4facbac34b8ccd11e
Pablo Balbi <pabbalbi@amazon.com>
2021-05-24 01:47:55 -0300

    Formatting. Float center

--- breadculator/js/index.js
+++ breadculator/js/index.js
@@ -29,7 +29,7 @@ var actons = {
 }
 
 var FlourIngredient = {
-    view: function({}) {
+    view: function ({ }) {
         return m("div.row", [
             m(".col", "Harina"),
             m("input[type=number].col text-center", {
@@ -42,31 +42,31 @@ var FlourIngredient = {
 }
 
 var ComputedIngredient = {
-    view: function({attrs}) {
+    view: function ({ attrs }) {
         return m("div.row", [
             m(".col", attrs.name),
             m(".col text-center", computed[attrs.ingredient] + " gr"),
-            m(".col text-center text-muted", state.ratios[attrs.ingredient]*100 + " %"),
+            m(".col text-center text-muted", state.ratios[attrs.ingredient] * 100 + " %"),
         ]);
     }
 }
 
 var Calculator = {
-    view: function() {
+    view: function () {
         return m("div.card", { style: "width: 40rem;" }, [
             m("div.card-body", [
-                m("h5.card-title", "Breadculator"),
-                m("p.card-text", "Calculadora de recetas de pan"),
+                m("h5.card-title text-center", "Breadculator"),
+                m("p.card-text text-center", "Calculadora de recetas de pan"),
                 // Ingredients
                 m("ul.list-group list-group-flush",
                     m("li.list-group-item",
                         m(FlourIngredient)),
                     m("li.list-group-item",
-                        m(ComputedIngredient, {name: "Agua", ingredient: "water"})),
+                        m(ComputedIngredient, { name: "Agua", ingredient: "water" })),
                     m("li.list-group-item",
-                        m(ComputedIngredient, {name: "Levadura", ingredient: "yeast"})),
+                        m(ComputedIngredient, { name: "Levadura", ingredient: "yeast" })),
                     m("li.list-group-item",
-                        m(ComputedIngredient, {name: "Sal", ingredient: "salt"})),
+                        m(ComputedIngredient, { name: "Sal", ingredient: "salt" })),
                 )
             ])
         ]);
@@ -76,7 +76,9 @@ var Calculator = {
 
 var Main = {
     view: function () {
-        return m("div.container pt-3", m(Calculator));
+        return m("div.container pt-3",
+            m("div.row justify-content-md-center",
+                m(Calculator)));
     }
 }
 
